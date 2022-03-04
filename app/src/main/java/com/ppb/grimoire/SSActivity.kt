35dfd8c54aa3d4f417a7863d5199a25c3b7a46c7
Jeddi212 <jedediahf51@gmail.com>
2022-03-05 04:04:51 +0700

    Fix Action Bar Bug 🐛

--- app/src/main/java/com/ppb/grimoire/SSActivity.kt
+++ app/src/main/java/com/ppb/grimoire/SSActivity.kt
@@ -1,11 +1,21 @@
 package com.ppb.grimoire
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SSActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({ //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
+        }, 4500)
     }
 }
