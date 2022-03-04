9a7b5b789a138996f1dabcbff2bc8c87764c5064
Jeddi212 <jedediahf51@gmail.com>
2022-03-05 03:28:34 +0700

    Add Splash Screen ✨

---
+++ app/src/main/java/com/ppb/grimoire/SSActivity.kt
@@ -0,0 +1,11 @@
+package com.ppb.grimoire
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SSActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
