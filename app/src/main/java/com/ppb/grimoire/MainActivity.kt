01428e48468c7bfc0e9e8fe4e80eca317bd541d2
Jeddi212 <jedediahf51@gmail.com>
2022-03-05 17:58:57 +0700

    Add nav_graph.xml

--- app/src/main/java/com/ppb/grimoire/MainActivity.kt
+++ app/src/main/java/com/ppb/grimoire/MainActivity.kt
@@ -11,25 +11,37 @@ import com.ppb.grimoire.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+//        val navController = findNavController(R.id.nav_fragment)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        bottomNavigationView.setupWithNavController(navController)
     }
 }
