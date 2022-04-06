f4d33d9369f1f41ccd50a2acd5d853360fa675cf
Jeddi212 <jedediahf51@gmail.com>
2022-04-06 16:56:22 +0700

    MainActivity.kt 🥖🍖🍙🍚🥩

--- app/src/main/java/com/ppb/grimoire/MainActivity.kt
+++ app/src/main/java/com/ppb/grimoire/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.ppb.grimoire
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.ppb.grimoire.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,8 +18,6 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        // Dynamic Action Bar, Buang aja kali ya? pasang textview manual aja nanti?
-        // Soalnya nutupin content
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.notesFragment,
