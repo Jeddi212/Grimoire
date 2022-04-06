0ad29c72091d43bd33773182119f1b5558758f9c
Jeddi212 <jedediahf51@gmail.com>
2022-04-06 13:12:14 +0700

    Remove Unused Import

--- app/src/main/java/com/ppb/grimoire/MainActivity.kt
+++ app/src/main/java/com/ppb/grimoire/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.ppb.grimoire
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
