4f2246c11423d4dbddc4be1cba8f57b8d99a671a
Jeddi212 <jedediahf51@gmail.com>
2022-04-22 23:52:14 +0700

    Create BackupActivity.kt

---
+++ app/src/main/java/com/ppb/grimoire/BackupActivity.kt
@@ -0,0 +1,11 @@
+package com.ppb.grimoire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BackupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backup)
+    }
+}
