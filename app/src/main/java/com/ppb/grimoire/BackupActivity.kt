97ce4280f429f39207ca625f865517ad182b3862
Jeddi212 <jedediahf51@gmail.com>
2022-04-23 00:33:41 +0700

    Add BackupBtn & RestoreBtn

--- app/src/main/java/com/ppb/grimoire/BackupActivity.kt
+++ app/src/main/java/com/ppb/grimoire/BackupActivity.kt
@@ -1,11 +1,40 @@
 package com.ppb.grimoire
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class BackupActivity : AppCompatActivity() {
+
+class BackupActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var backupBtn: Button
+    private lateinit var restoreBtn: Button
+
+    private fun init() {
+        backupBtn = findViewById(R.id.backup_btn)
+        restoreBtn = findViewById(R.id.restore_btn)
+
+        backupBtn.setOnClickListener(this)
+        restoreBtn.setOnClickListener(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_backup)
+
+        init()
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.backup_btn -> {
+                Log.i("JEDDI", "Masuk Backup Button ::: 18")
+            }
+            R.id.restore_btn -> {
+                Log.i("JEDDI", "Masuk Restore Button ::: 22")
+            }
+        }
     }
 }
